2d8888a316065d00ae56e9c39d1a3c628cbe5b02
ADDB <addb1234567890@gmail.com>
2023-10-04 23:12:28 +0200

    Made name oppsie

--- InkboundModEnabler/Main.cs
+++ InkboundModEnabler/Main.cs
@@ -12,7 +12,7 @@ namespace InkboundModEnabler {
     [BepInPlugin(PLUGIN_GUID, PLUGIN_NAME, PLUGIN_VERSION)]
     class InkboundModEnabler : BaseUnityPlugin {
         public const string PLUGIN_GUID = "ADDB.InkboundModEnabler";
-        public const string PLUGIN_NAME = "Force Offline";
+        public const string PLUGIN_NAME = "Inkbound Mod Enabler";
         public const string PLUGIN_VERSION = "1.1.0";
         public static Harmony HarmonyInstance => new Harmony(PLUGIN_GUID);
         private void Awake() {
