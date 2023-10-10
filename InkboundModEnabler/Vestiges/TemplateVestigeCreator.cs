659d9f5095461b739571441cd225889fdd0d65c3
ADDB <addb1234567890@gmail.com>
2023-10-10 12:38:22 +0200

    Custom Vestiges now save between run pausing; It is now possible to use Vestige Template to somewhat modify existing Vestiges.

--- InkboundModEnabler/Vestiges/TemplateVestigeCreator.cs
+++ InkboundModEnabler/Vestiges/TemplateVestigeCreator.cs
@@ -69,71 +69,95 @@ namespace InkboundModEnabler.Vestiges {
                 } catch (Exception ex) {
                     InkboundModEnabler.log.LogError($"Tried parsing custom vestige {file} and encountered the error:\n{ex}");
                 }
-                if (template == null) continue;
-                var newVestige = VestigeUtils.createBlankEquipmentData();
-                if (!template.Description.IsNullOrWhiteSpace()) {
-                    newVestige.description = template.Description;
-                } else {
-                    InkboundModEnabler.log.LogWarning($"Custom Vestige {file} is missing a Description!");
+                if (template != null) {
+                    RegisterNewTemplate(template, file);
                 }
-                if (!template.DisplayName.IsNullOrWhiteSpace()) {
-                    newVestige.equipmentName = template.DisplayName;
-                } else {
-                    InkboundModEnabler.log.LogWarning($"Custom Vestige {file} is missing a Display Name!");
-                    newVestige.equipmentName ??= template.InternalName;
-                }
-                newVestige.m_Name = template.InternalName ?? template.DisplayName;
+            }
+        }
+
+        public static void RegisterNewTemplate(TemplateVestige template, string originForLog) {
+            var existing = VestigeUtils.getEquipmentDataByName(template.InternalName) ?? VestigeUtils.getEquipmentDataByGUID(template.GUID);
+            ShinyShoe.Ares.SharedSOs.EquipmentData newVestige;
+            if (existing != null) {
+                newVestige = existing;
+            } else {
+                newVestige = VestigeUtils.createBlankEquipmentData();
+            }
+            if (!template.Description.IsNullOrWhiteSpace()) {
+                newVestige.description = template.Description;
+            } else {
+                InkboundModEnabler.log.LogWarning($"Custom Vestige {originForLog} is missing a Description!");
+            }
+            if (!template.DisplayName.IsNullOrWhiteSpace()) {
+                newVestige.equipmentName = template.DisplayName;
+            } else {
+                InkboundModEnabler.log.LogWarning($"Custom Vestige {originForLog} is missing a Display Name!");
+                newVestige.equipmentName ??= template.InternalName;
+            }
+            newVestige.m_Name = template.InternalName ?? template.DisplayName;
+            if (existing == null) {
                 if (!template.GUID.IsNullOrWhiteSpace()) {
                     newVestige.guid = template.GUID;
+                    newVestige.id = template.GUID;
                 } else {
-                    InkboundModEnabler.log.LogWarning($"Custom Vestige {file} is missing a GUID!");
+                    InkboundModEnabler.log.LogWarning($"Custom Vestige {originForLog} is missing a GUID!");
                     newVestige.guid = ShinyShoe.GuidProvider.Runtime.CreateLongGuid();
+                    newVestige.id = ShinyShoe.GuidProvider.Runtime.CreateLongGuid();
                 }
-                if (!template.Rarity.IsNullOrWhiteSpace() && stringToRarity.ContainsKey(template.Rarity.ToLower())) {
-                    newVestige.rarity = stringToRarity[template.Rarity.ToLower()];
-                } else {
-                    InkboundModEnabler.log.LogWarning($"Custom Vestige {file} is missing a Rarity!");
-                }
+            }
+            if (!template.Rarity.IsNullOrWhiteSpace() && stringToRarity.ContainsKey(template.Rarity.ToLower())) {
+                newVestige.rarity = stringToRarity[template.Rarity.ToLower()];
+            } else {
+                InkboundModEnabler.log.LogWarning($"Custom Vestige {originForLog} is missing a Rarity!");
+            }
+            if (existing == null) {
                 if (!template.SmallIconPath.IsNullOrWhiteSpace()) {
                     newVestige.AddSmallSprite($"{newVestige.m_Name}_SmallSprite", template.SmallIconPath);
                 } else {
-                    InkboundModEnabler.log.LogWarning($"Custom Vestige {file} is missing a SmallIconPath!");
+                    InkboundModEnabler.log.LogWarning($"Custom Vestige {originForLog} is missing a SmallIconPath!");
                 }
-                if (!template.statChanges.IsNullOrEmpty()) {
-                    foreach (var sc in template.statChanges) {
-                        ShinyShoe.Ares.SharedSOs.StatEntryData sed = new();
-                        ShinyShoe.Ares.SharedSOs.StatData sd = VestigeUtils.getStatDataByGUID(sc.identifier) ?? VestigeUtils.getStatDataByDisplayName(sc.identifier) ?? VestigeUtils.getStatDataByName(sc.identifier);
-                        if (sd == null) {
-                            InkboundModEnabler.log.LogWarning($"Custom Vestige {file} references unknown stat {sc.identifier}!");
-                            continue;
-                        }
-                        sed.entryStatData = sd;
-                        sed.entryValue = sc.value;
-                        newVestige.statEntries.statEntries.Add(sed);
+            }
+            if (!template.statChanges.IsNullOrEmpty()) {
+                foreach (var sc in template.statChanges) {
+                    ShinyShoe.Ares.SharedSOs.StatEntryData sed = new();
+                    ShinyShoe.Ares.SharedSOs.StatData sd = VestigeUtils.getStatDataByGUID(sc.identifier) ?? VestigeUtils.getStatDataByDisplayName(sc.identifier) ?? VestigeUtils.getStatDataByName(sc.identifier);
+                    if (sd == null) {
+                        InkboundModEnabler.log.LogWarning($"Custom Vestige {originForLog} references unknown stat {sc.identifier}!");
+                        continue;
                     }
+                    sed.entryStatData = sd;
+                    sed.entryValue = sc.value;
+                    newVestige.statEntries.statEntries.Add(sed);
                 }
-                if (!template.statusEffects.IsNullOrEmpty()) {
-                    foreach (var effect in template.statusEffects) {
-                        ShinyShoe.Ares.SharedSOs.StatusEffectEntryData ssed = new();
-                        ShinyShoe.Ares.SharedSOs.StatusEffectData ssd = VestigeUtils.getStatusEffectDataByGUID(effect.identifier) ?? VestigeUtils.getStatusEffectDataByName(effect.identifier) ?? VestigeUtils.getStatusEffectDataByDisplayName(effect.identifier);
-                        if (ssd == null) {
-                            InkboundModEnabler.log.LogWarning($"Custom Vestige {file} references unknown status effect {effect.identifier}!");
-                            continue;
-                        }
-                        ssed.statusEffect = ssd;
-                        ssed.stackCount = effect.stackCount;
-                        ssed.isPrimaryAttribute = effect.isPrimaryAttribute;
-                        newVestige.statusEffectEntries.StatusEffectEntries.Add(ssed);
+            }
+            if (!template.statusEffects.IsNullOrEmpty()) {
+                foreach (var effect in template.statusEffects) {
+                    ShinyShoe.Ares.SharedSOs.StatusEffectEntryData ssed = new();
+                    ShinyShoe.Ares.SharedSOs.StatusEffectData ssd = VestigeUtils.getStatusEffectDataByGUID(effect.identifier) ?? VestigeUtils.getStatusEffectDataByName(effect.identifier) ?? VestigeUtils.getStatusEffectDataByDisplayName(effect.identifier);
+                    if (ssd == null) {
+                        InkboundModEnabler.log.LogWarning($"Custom Vestige {originForLog} references unknown status effect {effect.identifier}!");
+                        continue;
                     }
+                    ssed.statusEffect = ssd;
+                    ssed.stackCount = effect.stackCount;
+                    ssed.isPrimaryAttribute = effect.isPrimaryAttribute;
+                    newVestige.statusEffectEntries.StatusEffectEntries.Add(ssed);
                 }
+            }
+            if (existing == null) {
                 newVestige.RegisterNewVestige();
                 if (template.Weight > 0) {
                     newVestige.AddToLootList("AllVestigeList", template.Weight);
                 } else {
-                    InkboundModEnabler.log.LogWarning($"Custom Vestige {file} Weight is <= 0!");
+                    InkboundModEnabler.log.LogWarning($"Custom Vestige {originForLog} Weight is <= 0!");
                     newVestige.AddToLootList("AllVestigeList", 1);
                 }
                 InkboundModEnabler.log.LogInfo($"Enabled custom vestige {newVestige.Name}");
+            } else {
+                if (template.Weight > 1) {
+                    newVestige.ModifyInLootList("AllVestigeList", template.Weight);
+                }
+                InkboundModEnabler.log.LogInfo($"Modified base vestige {newVestige.Name} ({newVestige.name})");
             }
         }
     }
